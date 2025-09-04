281d2d2a3ba286b2dbd4b9d6678dadff768c74fc
hellofarooqui <farooqui.hello@gmail.com>
2025-09-04 07:15:22 +0400

    Changed font and icon size for each user

--- client/src/components/EnrolledUsers.jsx
+++ client/src/components/EnrolledUsers.jsx
@@ -5,14 +5,14 @@ const EnrolledUsers = ({ players }) => {
     <div className="w-full mt-6">
       <h3 className="text-2xl text-start font-bold">Enrolled Users <span className="text-[16px] bg-slate-200/20 px-2 py-1 border rounded-full">{players.length}</span></h3>
 
-      <ul className="mt-4 flex flex-col gap-y-4">
+      <ul className="mt-4 flex flex-col gap-y-4 text-lg">
         {players.map((player) => (
           <li
             key={player._id}
-            className="flex items-center bg-slate-200/20 gap-x-2 rounded-lg p-4"
+            className="flex items-center bg-slate-200/20 gap-x-2 rounded-lg px-4 py-2"
           >
             <span className="p-2  bg-gradient-to-br from-[#667eea]  to-[#764ba2] rounded-lg">
-              <User className="" />
+              <User className="" size={18}/>
             </span>
             {player.firstName} {player.lastName}
           </li>
