3764bcd048fb5bf20d16e7cde0e8bab5d1e84305
hellofarooqui <farooqui.hello@gmail.com>
2025-10-06 16:15:24 +0400

    updated enrolled user font size

--- client/src/components/EnrolledUsers.jsx
+++ client/src/components/EnrolledUsers.jsx
@@ -4,16 +4,16 @@ const EnrolledUsers = ({ players }) => {
   console.log("Enrolled players:", players);
   return (
     <div className="w-full rounded-[10px] bg-dark-gray overflow-hidden shadow-sm mt-2">
-      <h3 className="text-lg text-start font-bold bg-light-main-blue text-white  p-2 px-4 flex justify-between items-center">Enrolled Users <span className="">{players.length}</span></h3>
+      <h3 className="text-lg text-start font-bold bg-dark-blue/20 border-dark-blue/50 text-white  p-2 px-4 flex justify-between items-center">Enrolled Users <span className="">{players.length}</span></h3>
 
-      <ul className="my-2 flex flex-col gap-y-4 text-lg">
+      <ul className="my-2 flex flex-col  text-lg">
         {players.map((player) => (
           <li
             key={player._id}
-            className="flex items-center  text-dark-white gap-x-2 rounded-lg px-4 py-2"
+            className="flex items-center  text-dark-white gap-x-2 rounded-lg px-4 py-2 text-base"
           >
-            <span className="p-2 border border-light-text-dull-02 rounded-full">
-              <User className="text-dark-white" size={18}/>
+            <span className="p-1 border border-light-text-dull-02 rounded-full">
+              <User className="text-dark-white" size={16}/>
             </span>
             {player.firstName} {player.lastName}
           </li>
